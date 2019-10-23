3fa97bd32e855300978f04f2154652f57066934f
ssfdust <ssfdust@gmail.com>
2019-10-23 17:42:03 +0800

    fix code style

--- app/backtasks/sighandler.py
+++ app/backtasks/sighandler.py
@@ -29,9 +29,8 @@ from celery import signals
 from app.utils.formatters import celery_worker_formatter as worker_formatter
 
 
-@signals.task_prerun.connect
-def handle_task_prerun(sender=None, task_id=None, state=None, **kwargs):
-    """处理任务预运行信号"""
+def _store_task_info(sender):
+    """保存任务信息"""
     from app.extensions import celery_ext
     from app.extensions.celerybackend import models
 
@@ -45,111 +44,136 @@ def handle_task_prerun(sender=None, task_id=None, state=None, **kwargs):
         item['state'] = 'run'
         item.pop('type')
         models.Tasks.objects(id=_id).upsert_one(**item)
+
+
+@signals.task_prerun.connect
+def handle_task_prerun(sender=None, task_id=None, state=None, **kwargs):
+    """处理任务预运行信号"""
+    #  from app.extensions import celery_ext
+    #  from app.extensions.celerybackend import models
+    #
+    #  app = celery_ext.get_celery_app()
+    #  inspect = app.control.inspect()
+    #  info = inspect.query_task(sender.request.id)
+    #  lst = worker_formatter(info)
+    #
+    #  for item in lst:
+    #      _id = item.pop('id')
+    #      item['state'] = 'run'
+    #      item.pop('type')
+    #      models.Tasks.objects(id=_id).upsert_one(**item)
+    _store_task_info(sender)
 
 
 @signals.task_failure.connect
 def handle_task_failure(sender=None, task_id=None, state=None, **kwargs):
     """处理任务预失败信号"""
-    from app.extensions import celery_ext
-    from app.extensions.celerybackend import models
-
-    app = celery_ext.get_celery_app()
-    inspect = app.control.inspect()
-    info = inspect.query_task(sender.request.id)
-    lst = worker_formatter(info)
-
-    for item in lst:
-        _id = item.pop('id')
-        item['state'] = 'fail'
-        item.pop('type')
-        models.Tasks.objects(id=_id).upsert_one(**item)
+    #  from app.extensions import celery_ext
+    #  from app.extensions.celerybackend import models
+    #
+    #  app = celery_ext.get_celery_app()
+    #  inspect = app.control.inspect()
+    #  info = inspect.query_task(sender.request.id)
+    #  lst = worker_formatter(info)
+    #
+    #  for item in lst:
+    #      _id = item.pop('id')
+    #      item['state'] = 'fail'
+    #      item.pop('type')
+    #      models.Tasks.objects(id=_id).upsert_one(**item)
+    _store_task_info(sender)
 
 
 @signals.task_success.connect
 def handle_task_success(sender=None, task_id=None, state=None, **kwargs):
     """处理任务成功信号"""
-    from app.extensions import celery_ext
-    from app.extensions.celerybackend import models
-
-    app = celery_ext.get_celery_app()
-    inspect = app.control.inspect()
-    info = inspect.query_task(sender.request.id)
-    lst = worker_formatter(info)
-
-    for item in lst:
-        _id = item.pop('id')
-        item['state'] = 'success'
-        item.pop('type')
-        models.Tasks.objects(id=_id).upsert_one(**item)
+    #  from app.extensions import celery_ext
+    #  from app.extensions.celerybackend import models
+    #
+    #  app = celery_ext.get_celery_app()
+    #  inspect = app.control.inspect()
+    #  info = inspect.query_task(sender.request.id)
+    #  lst = worker_formatter(info)
+    #
+    #  for item in lst:
+    #      _id = item.pop('id')
+    #      item['state'] = 'success'
+    #      item.pop('type')
+    #      models.Tasks.objects(id=_id).upsert_one(**item)
+    _store_task_info(sender)
 
 
 @signals.task_retry.connect
 def handle_task_retry(sender=None, task_id=None, state=None, **kwargs):
     """处理任务重试信号"""
-    from app.extensions import celery_ext
-    from app.extensions.celerybackend import models
-
-    app = celery_ext.get_celery_app()
-    inspect = app.control.inspect()
-    info = inspect.query_task(sender.request.id)
-    lst = worker_formatter(info)
-
-    for item in lst:
-        _id = item.pop('id')
-        item['state'] = 'run'
-        item.pop('type')
-        models.Tasks.objects(id=_id).upsert_one(**item)
+    #  from app.extensions import celery_ext
+    #  from app.extensions.celerybackend import models
+    #
+    #  app = celery_ext.get_celery_app()
+    #  inspect = app.control.inspect()
+    #  info = inspect.query_task(sender.request.id)
+    #  lst = worker_formatter(info)
+    #
+    #  for item in lst:
+    #      _id = item.pop('id')
+    #      item['state'] = 'run'
+    #      item.pop('type')
+    #      models.Tasks.objects(id=_id).upsert_one(**item)
+    _store_task_info(sender)
 
 
 @signals.task_revoked.connect
 def handle_task_revoked(sender=None, task_id=None, state=None, **kwargs):
     """处理任务撤销信号"""
-    from app.extensions import celery_ext
-    from app.extensions.celerybackend import models
-
-    app = celery_ext.get_celery_app()
-    inspect = app.control.inspect()
-    info = inspect.query_task(sender.request.id)
-    lst = worker_formatter(info)
-
-    for item in lst:
-        _id = item.pop('id')
-        item['state'] = 'fail'
-        item.pop('type')
-        models.Tasks.objects(id=_id).upsert_one(**item)
+    #  from app.extensions import celery_ext
+    #  from app.extensions.celerybackend import models
+    #
+    #  app = celery_ext.get_celery_app()
+    #  inspect = app.control.inspect()
+    #  info = inspect.query_task(sender.request.id)
+    #  lst = worker_formatter(info)
+    #
+    #  for item in lst:
+    #      _id = item.pop('id')
+    #      item['state'] = 'fail'
+    #      item.pop('type')
+    #      models.Tasks.objects(id=_id).upsert_one(**item)
+    _store_task_info(sender)
 
 
 @signals.task_rejected.connect
 def handle_task_rejected(sender=None, task_id=None, state=None, **kwargs):
     """处理任务拒绝信号"""
-    from app.extensions import celery_ext
-    from app.extensions.celerybackend import models
-
-    app = celery_ext.get_celery_app()
-    inspect = app.control.inspect()
-    info = inspect.query_task(sender.request.id)
-    lst = worker_formatter(info)
-
-    for item in lst:
-        _id = item.pop('id')
-        item['state'] = 'fail'
-        item.pop('type')
-        models.Tasks.objects(id=_id).upsert_one(**item)
+    #  from app.extensions import celery_ext
+    #  from app.extensions.celerybackend import models
+    #
+    #  app = celery_ext.get_celery_app()
+    #  inspect = app.control.inspect()
+    #  info = inspect.query_task(sender.request.id)
+    #  lst = worker_formatter(info)
+    #
+    #  for item in lst:
+    #      _id = item.pop('id')
+    #      item['state'] = 'fail'
+    #      item.pop('type')
+    #      models.Tasks.objects(id=_id).upsert_one(**item)
+    _store_task_info(sender)
 
 
 @signals.task_unknown.connect
 def handle_task_unknown(sender=None, task_id=None, state=None, **kwargs):
     """处理任务未知信号"""
-    from app.extensions import celery_ext
-    from app.extensions.celerybackend import models
-
-    app = celery_ext.get_celery_app()
-    inspect = app.control.inspect()
-    info = inspect.query_task(sender.request.id)
-    lst = worker_formatter(info)
-
-    for item in lst:
-        _id = item.pop('id')
-        item['state'] = 'fail'
-        item.pop('type')
-        models.Tasks.objects(id=_id).upsert_one(**item)
+    #  from app.extensions import celery_ext
+    #  from app.extensions.celerybackend import models
+    #
+    #  app = celery_ext.get_celery_app()
+    #  inspect = app.control.inspect()
+    #  info = inspect.query_task(sender.request.id)
+    #  lst = worker_formatter(info)
+    #
+    #  for item in lst:
+    #      _id = item.pop('id')
+    #      item['state'] = 'fail'
+    #      item.pop('type')
+    #      models.Tasks.objects(id=_id).upsert_one(**item)
+    _store_task_info(sender)
