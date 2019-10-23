81258472a0d9a6cfc65defc71137a8246bf93c04
ssfdust <ssfdust@gmail.com>
2019-10-23 17:46:03 +0800

    fix code style

--- app/static/js/pages/celerybeat.js
+++ app/static/js/pages/celerybeat.js
@@ -1,25 +1,25 @@
-$('#fa_modal_window').on('shown.bs.modal', function () {
-    var beattype = $('label[for=type').parent().parent();
-    var crontab = $('label[for=crontab]').parent().parent();
-    var interval = $('label[for=interval]').parent();
-    var form = $('form[role=form]');
+$("#fa_modal_window").on("shown.bs.modal", function () {
+    var beattype = $("label[for=type").parent().parent();
+    var crontab = $("label[for=crontab]").parent().parent();
+    var interval = $("label[for=interval]").parent();
+    var form = $("form[role=form]");
     var formarr = form.serializeArray();
-    var curtype = '';
+    var curtype = "";
     for (i=0;i < formarr.length;i++){
-        if (formarr[i]['name'] === 'type') {
-            curtype = formarr[i]['value'];
+        if (formarr[i].name === "type") {
+            curtype = formarr[i].value;
         }
     }
     interval.remove();
     crontab.remove();
-    if (curtype === 'crontab'){
+    if (curtype === "crontab"){
         crontab.insertAfter(beattype);
     } else {
         interval.insertAfter(beattype);
-    };
-    $('select#type').change(function(data) {
+    }
+    $("select#type").change(function(data) {
         var val = data.currentTarget.value;
-        if (val === 'crontab'){
+        if (val === "crontab"){
             interval.remove();
             crontab.insertAfter(beattype);
         } else {
--- app/static/js/pages/profile.js
+++ app/static/js/pages/profile.js
@@ -1,25 +1,25 @@
 const Toast = Swal.mixin({
   toast: true,
-  position: 'top-end',
+  position: "top-end",
   showConfirmButton: false,
   timer: 3000
 });
 
-$('#avator-overlay').on('click', function(){
+$("#avator-overlay").on("click", function(){
   $("#fileToUpload").click();
 })
-$('#avator-overlay').hover(function(){
-  $('#avator-overlay').animate({opacity: 1}, 300);
+$("#avator-overlay").hover(function(){
+  $("#avator-overlay").animate({opacity: 1}, 300);
 }, function () {
-  $('#avator-overlay').animate({opacity: 0.0}, 300);
+  $("#avator-overlay").animate({opacity: 0.0}, 300);
 })
 
-$('#fileToUpload').on('change', function(){
+$("#fileToUpload").on("change", function(){
   var formData = new FormData();
   formData.append("file",$("#fileToUpload")[0].files[0]);
   $.ajax({  
-        url : '/admin/profile/upload-avator',  
-        type : 'POST',  
+        url : "/admin/profile/upload-avator",  
+        type : "POST",  
         data : formData,  
         // 告诉jQuery不要去处理发送的数据
         processData : false, 
@@ -27,8 +27,8 @@ $('#fileToUpload').on('change', function(){
         contentType : false,
         success : function(res) { 
             if(res.code === 0){
-              var imgUrl = $('.usr-avator').attr('src');
-              $('.usr-avator').attr('src', imgUrl + '?' + new Date().getTime());
+              var imgUrl = $(".usr-avator").attr("src");
+              $(".usr-avator").attr("src", imgUrl + "?" + new Date().getTime());
             }else{
                 console.log("失败");
             }
@@ -39,33 +39,33 @@ $('#fileToUpload').on('change', function(){
     }); 
 })
 
-$('#savePassword').on('click', function(){
-    if ($('#passwdForm')[0].checkValidity() === true)
+$("#savePassword").on("click", function(){
+    if ($("#passwdForm")[0].checkValidity() === true)
     {
-        var data = $('#passwdForm').serializeJSON();
+        var data = $("#passwdForm").serializeJSON();
         console.log(data);
         $.ajax({
           type: "patch",
           dataType: "json",
-          url: '/admin/profile/set-passwd',
+          url: "/admin/profile/set-passwd",
           data: JSON.stringify(data),
           contentType: "application/json; charset=utf-8",
           success: function(data) {
             if (data.code == 1){
                 Toast.fire({
-                  type: 'error',
-                  title: '初始密码错误'
+                  type: "error",
+                  title: "初始密码错误"
                 });
             } else {
                 Toast.fire({
-                  type: 'success',
-                  title: '修改成功'
+                  type: "success",
+                  title: "修改成功"
                 });
-                $('#modal-default').modal('toggle');
+                $("#modal-default").modal("toggle");
             }
           }
         });
     } else {
-      $('#passwdForm')[0].reportValidity();
+      $("#passwdForm")[0].reportValidity();
     }
 })
